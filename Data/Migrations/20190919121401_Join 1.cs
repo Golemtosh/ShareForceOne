d29c8f1df66c44fd6b12ab0a71be6b628d7620b5
Tomas Slättman <tomas.aw@ninu.se>
2019-09-19 14:14:25 +0200

    Join first test

---
+++ Data/Migrations/20190919121401_Join 1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShareForceOne.Data.Migrations
+{
+    public partial class Join1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JoinTripModel",
+                columns: table => new
+                {
+                    joinTripId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TripId = table.Column<int>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    JoinTripNotes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JoinTripModel", x => x.joinTripId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JoinTripModel");
+        }
+    }
+}
---
+++ Models/JoinTripModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareForceOne.Models
+{
+    public class JoinTripModel
+    {
+        [Key]
+        public int joinTripId { get; set; }
+        [DisplayName("Trip Number: ")]
+        public int TripId { get; set; }
+        public string UserId { get; set; }
+        [DisplayName("Notes")]
+        public string JoinTripNotes { get; set; }
+
+    }
+}
